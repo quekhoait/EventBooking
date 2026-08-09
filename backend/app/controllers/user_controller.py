38a9ddb1e1bcbbee4a56fcd02f4ad437b247d0cb
Nguyễn Duy Hoàng <Nguyenduyhoang0605@gmail.com>
2026-08-10 00:05:53 +0700

    N20-36 [BE] Tạo hồ sơ đơn vị tổ chức.

--- backend/app/controllers/user_controller.py
+++ backend/app/controllers/user_controller.py
@@ -35,3 +35,42 @@ def get_profile():
         },
         status_code=200,
     )
+
+
+@user_api.route("/profile", methods=["PUT", "PATCH"])
+@jwt_required()
+def update_profile():
+    current_user_id = get_jwt_identity()
+    profile_data = request.get_json()
+
+    if profile_data is None:
+        return NewPackage(
+            status=StatusResponse.ERROR,
+            message="Invalid input data: No JSON payload provided",
+            status_code=400,
+        )
+
+    user = user_services.update_profile(
+        user_id=current_user_id, profile_data=profile_data
+    )
+
+    if not user:
+        return NewPackage(
+            status=StatusResponse.ERROR,
+            message="User not found",
+            status_code=404,
+        )
+
+    return NewPackage(
+        status=StatusResponse.SUCCESS,
+        message="Profile updated successfully",
+        data={
+            "id": user.id,
+            "email": user.email,
+            "username": user.username,
+            "fullname": user.full_name,
+            "number_phone": user.phone_number,
+            "is_verified": user.is_verified,
+        },
+        status_code=200,
+    )
