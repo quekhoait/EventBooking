6dcff345e5d06b534a87d5f5768afff2a82e03e8
quekhoait <166801979+quekhoait@users.noreply.github.com>
2026-09-10 20:01:11 +0700

    update

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,19 @@
+from gevent import monkey
+
+monkey.patch_all()  
+
+import os
 from app import create_app, db, socketio
 from app.models import *
 
 app = create_app()
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
 
-    print(app.url_map)
-    socketio.run(app, debug=True, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+  with app.app_context():
+    db.create_all()
+
+  print(app.url_map)
+
+  port = int(os.environ.get("PORT", 8000))
+
+  socketio.run(app, host="0.0.0.0", port=port)
